d127562fe635fe7041e53afc854f9403fbbef9fc
Karimov <121497820+AGK-1@users.noreply.github.com>
2025-03-13 01:05:25 +0400

    Update Program.cs
    up

--- Program.cs
+++ Program.cs
@@ -5,26 +5,24 @@ using SQLE_sam;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(options => 
+builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Добавляем настройку HTTPS здесь, до создания объекта app
-builder.Services.AddHttpsRedirection(options => { options.HttpsPort = 443; });
-
+// Add other necessary services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Swagger UI və Swagger JSON aktivləşdirilməsi
+// Swagger UI and Swagger JSON configuration
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(); // Swagger UI aktivləşdirilir
+    app.UseSwaggerUI(); // Activate Swagger UI
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); // Ensure HTTPS redirection happens only once
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
